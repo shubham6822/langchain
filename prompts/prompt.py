5901e94cf5bc839e3a817ac237a88b2b86f45614
Shubham <sharmashubham6822@gmail.com>
2025-12-17 17:23:24 +0530

    chatbot

--- prompts/prompt.py
+++ prompts/prompt.py
@@ -1,3 +1,15 @@
 from dotenv import load_dotenv
-from strea
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
 load_dotenv()
+
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    result = model.invoke(user_input)
+    print("AI:  ",result.content)

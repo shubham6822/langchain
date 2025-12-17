18f3e30f22a4aaa4c57c5417d527731b3df8b4e3
Shubham <sharmashubham6822@gmail.com>
2025-12-17 17:27:25 +0530

    role base

--- prompts/prompt.py
+++ prompts/prompt.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 
 load_dotenv()
@@ -7,9 +8,17 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
+messages=[
+    SystemMessage(content='You are a helpful assistant'),
+]
+
 while True:
-    user_input = input("You: ")
+    user_input:str = input("You: ")
     if user_input == "exit":
         break
-    result = model.invoke(user_input)
+    messages.append(HumanMessage(content=user_input))
+    result = model.invoke(messages)
+    messages.append(AIMessage(content=result.content))
     print("AI:  ",result.content)
+
+print("Messages: ",messages)
